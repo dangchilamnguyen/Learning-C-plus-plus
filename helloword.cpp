018e40da935e8a34f2ca14950f3a8e611d5b1f36
Dang Chi Lam Nguyen <dangchilamnguyen@gmail.com>
2026-07-11 16:17:07 +1000

    Calculating the hypotenuse.

---
+++ helloword.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main()
+{
+    system("chcp 65001 > nul"); // Thêm dòng này để sửa lỗi font tiếng Việt
+    double a;
+    double b;
+    double c;
+
+    std::cout << "cạnh 1: ";
+    std::cin >> a; // Nhập xong cạnh 1 mới hỏi tiếp cạnh 2
+
+    std::cout << "cạnh 2: ";
+    std::cin >> b;
+
+    c = sqrt(pow(a, 2) + pow(b, 2));
+    std::cout << "Cạnh huyền = " << c << '\n';
+
+    return 0;
+}
